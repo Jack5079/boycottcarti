363202e57392ba4fb86fe726a3664f38093f6566
Jack W. <git@jack.cab>
2024-12-31 21:38:05 -0500

    fix: LMAOOOOO
    Fixes #1

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,44 @@
+use std::thread;
+
 use mpris::{Event, PlayerFinder};
 
 fn main() {
     let player_finder = PlayerFinder::new().expect("Could not connect to D-Bus");
 
-    for player in player_finder.iter_players().unwrap() {
-        let player = player.unwrap();
-        if player
-            .get_metadata()
-            .unwrap()
-            .artists()
-            .unwrap_or_default()
-            .contains(&"Playboi Carti")
-        {
-            player.next().unwrap();
-        }
-        for event in player.events().unwrap().flat_map(|opt| opt) {
-            if let Event::TrackChanged(metadata) = event {
-                if metadata
-                    .artists()
-                    .unwrap_or_default()
-                    .contains(&"Playboi Carti")
-                {
-                    player.next().unwrap();
-                }
+    thread::scope(|scope| {
+        for player in player_finder.iter_players().unwrap() {
+            let player = player.unwrap();
+            if player
+                .get_metadata()
+                .unwrap()
+                .artists()
+                .unwrap_or_default()
+                .contains(&"Playboi Carti")
+            {
+                player.next().unwrap();
             }
+
+            let id = player.unique_name().to_owned();
+            scope.spawn(move || {
+                let plr = PlayerFinder::new()
+                    .expect("Could not connect to D-Bus")
+                    .iter_players()
+                    .unwrap()
+                    .flat_map(|opt| opt)
+                    .find(|p| p.unique_name() == id)
+                    .unwrap();
+                for event in plr.events().unwrap().flat_map(|opt| opt) {
+                    if let Event::TrackChanged(metadata) = event {
+                        if metadata
+                            .artists()
+                            .unwrap_or_default()
+                            .contains(&"Playboi Carti")
+                        {
+                            plr.next().unwrap();
+                        }
+                    }
+                }
+            });
         }
-    }
+    })
 }
